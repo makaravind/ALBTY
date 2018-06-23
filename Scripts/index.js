a1dbfc057046790db94bedf65e71d4e02bcb5c8a
AravindM <aravindkumar.metku@github.com>
2018-06-23 08:58:39 +0530

    1. Added better prompt cli 2. Changed config executor

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,6 +2,8 @@ const Promisify = require("bluebird");
 const fs = Promisify.promisifyAll(require("fs"));
 const request = require("request");
 const github = require("github-api");
+const prompt = require("prompt");
+const cliPromt = require("cli-input");
 
 function configure() {
   return fs.readFileAsync("conf.json", "utf8").then(conf => {
@@ -35,8 +37,12 @@ const getAllCommentsOnIssue = () => {
   });
 };
 
+const confContainer = (gh, conf) => {
+  return (fn, ...args) => confWraper({ gh, conf }, fn, args)
+}
+
 const confWraper = (conf, fn, ...args) => {
-  return fn(conf, ...args);
+  return fn(conf.conf, ...args);
 };
 
 const updateLog = ({ created_at: createdAt, body }) => {
@@ -51,7 +57,7 @@ const postNewCommentOnIssue = (conf, data) => {
   const options = {
     url: `${conf.GITHUB_API_URL}/repos/${conf.USERNAME}/${conf.REPO}/issues/${
       conf.ISSUE_NUMBER
-    }/comments`,
+      }/comments`,
     headers: {
       "User-Agent": `${conf.USERNAME}`,
       Authorization: `token ${conf.TOKEN}`
@@ -71,20 +77,31 @@ const postNewCommentOnIssue = (conf, data) => {
 };
 
 const getContentFromSrc = conf => {
+  /* gets last new post from the new array */
   return fs.readFileAsync(conf.SRC, "utf8").then(src => {
     // todo: add the popped to done array; refactor
     return JSON.parse(src).new.pop().content;
   });
 };
 
-configure().then(({ gh, conf }) => {
+const getSrcContent = conf => {
+  return fs.readFileAsync(conf.SRC, "utf8").then(src => {
+    return JSON.parse(src);
+  }); 
+};
+
+const configureAndPublishPostToGitHub = (executor) => {
   /**
    * get data from src, v1: json array markdown string, v2: google sheet
    * */
-  confWraper(conf, getContentFromSrc)
+  executor(getContentFromSrc)
     .then(content => {
       console.log("step 1: got content");
-      return confWraper(conf, postNewCommentOnIssue, content);
+      prompt.start();
+      prompt.get(['toContinue'], (err, result) => {
+        if (err || result.toContinue) throw Error('some error has occured or you wished to stop the publishing');
+        return executor(postNewCommentOnIssue, content);
+      })
     })
     .then(response => {
       console.log("step 2: got success response from github api");
@@ -96,4 +113,45 @@ configure().then(({ gh, conf }) => {
     .catch(err => {
       console.error("error adding new comment to the issue ", err);
     });
+  // });
+};
+
+const addNewContentSrc = (conf, content, number) => {
+  // add content to the src.json, with created_at
+  // console.log('number', number, 'content...', content);
+  _executor(getSrcContent).then(content => {
+    console.log('content from src file ', content);
+  });
+}
+
+const init = () => {
+  return configure().then(({ gh, conf }) => {
+    return confContainer(gh, conf)
+  });
+};
+
+init().then(executor => {
+  // put this in global context
+  // (fn, ...agrs)
+  _executor = executor;
+
+  prompt.start();
+  prompt.get(['command'], (err, result) => {
+    switch (result.command) {
+      case 'publish':
+        executor(configureAndPublishPostToGitHub);
+      case 'add':
+        prompt.start()
+        prompt.get(['number'], (err, result) => {
+          const number = result.number;
+          console.log('paste your content and then ctrl+d');
+          const ps = cliPromt();
+          ps.multiline((err, lines, raw) => {
+            const content = raw;
+            executor(addNewContentSrc, content, number);
+          });
+        });
+    }
+  })
 });
+
